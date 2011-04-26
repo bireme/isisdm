c09bc9c9d2a45f7c28ba745bd3744e6656ed3f78
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2011-04-26 15:05:22 -0300

    moving base28 to utils package

--- isis/model/couchdb.py
+++ isis/model/couchdb.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from scielobooks.utilities import base28
+from ..utils import base28
 from .mapper import Document, TextProperty
 import uuid
 import couchdbkit
