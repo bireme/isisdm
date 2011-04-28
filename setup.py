e3d545188b913cb03cee31dbd31c3340e9745d8b
Gustavo Fonseca <gustavofons@gmail.com>
2011-04-28 13:29:26 -0700

    incremented the package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requirements = ['colander']
 
-setup(name='isisdm', version='0.0.0',
+setup(name='isisdm', version='0.0.1',
       packages=find_packages(),
       long_description=README + "\n\n" + CHANGES,
       namespace_packages=['isis'],
