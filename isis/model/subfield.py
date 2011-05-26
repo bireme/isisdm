5ff051067fc4fb438759e6da1094dd20089d8fcc
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2011-05-26 17:09:05 -0300

    implementing CompositeTextProperty

--- isis/model/subfield.py
+++ isis/model/subfield.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from collections import namedtuple
 import re
 
 
@@ -54,15 +55,14 @@ def expand(content, subkeys=None):
 
 class CompositeString(object):
     ''' Represent an Isis field, with subfields, using
-        Python native datastructures
+    Python native datastructures
 
-        >>> author = CompositeString('John Tenniel^xillustrator',
-        ...                          subkeys='x')
-        >>> unicode(author)
-        u'John Tenniel^xillustrator'
-
+    >>> author = CompositeString('John Tenniel^xillustrator',
+    ... subkeys='x')
+    >>> unicode(author)
+    u'John Tenniel^xillustrator'
     '''
-
+    
     def __init__(self, isis_raw, subkeys=None, encoding=DEFAULT_ENCODING):
         if not isinstance(isis_raw, basestring):
             raise TypeError('%r value must be unicode or str instance' % isis_raw)
@@ -89,6 +89,49 @@ class CompositeString(object):
     def __str__(self):
         return str(self.__isis_raw)
 
+
+class CompositeTuple(object):
+    ''' Represent an Isis field, with subfields, using
+        Python native datastructures
+
+        >>> author = CompositeTuple( [('name','Braz, Marcelo'),('role','writer')] )
+        >>> print author['name']
+        Braz, Marcelo
+        >>> print author['role']
+        writer
+        >>> author
+        CompositeTuple((('name', 'Braz, Marcelo'), ('role', 'writer')))
+
+    '''
+
+    def __init__(self, value, subkeys=None):
+        if subkeys is None:
+            subkeys = [item[0] for item in value]
+        composite = namedtuple('Composite', subkeys)
+        try:
+            value_as_dict = dict(value)
+        except TypeError:
+            raise TypeError('%r value must be a key-value structure' % self)
+        
+        try:
+            self.value = composite(**value_as_dict)
+        except TypeError:
+            raise TypeError('%r got an unexpected keyword' % self)
+
+    def __getitem__(self, key):
+        return getattr(self.value,key)
+
+    def __repr__(self):
+        return "CompositeTuple(%s)" % str(self.items())
+
+    def items(self):
+        return tuple(zip(self.value._fields, self.value))
+
+    def __unicode__(self):
+        unicode(self.items())
+
+    def __str__(self):
+        str(self.items())
 
 
 def test():
