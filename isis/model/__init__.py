922fa3f80f0371c2cfa41c08e560f62ee8b06211
gustavofonseca <gustavofons@gmail.com>
2011-05-27 23:08:05 +0800

    added the new implementation of MultiCompositeTextProperty, based on CompositeTextProperty.the old implementation of this type has been renamed to MultiIsisCompositeTextProperty

--- isis/model/__init__.py
+++ isis/model/__init__.py
@@ -1,6 +1,7 @@
 # package
 from .mapper import Document
 from .mapper import TextProperty, MultiTextProperty
-from .mapper import CompositeTextProperty, IsisCompositeTextProperty, MultiCompositeTextProperty
+from .mapper import CompositeTextProperty, IsisCompositeTextProperty
+from .mapper import MultiIsisCompositeTextProperty, MultiCompositeTextProperty
 from .mapper import ReferenceProperty, FileProperty, BooleanProperty
 from .couchdb import CouchdbDocument
