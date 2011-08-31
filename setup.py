38dafb4d207ba936dfecbadaa25f0036051ffdb0
gustavofonseca <gustavofons@gmail.com>
2011-08-31 16:58:23 -0300

    version update

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ from setuptools import setup, find_packages
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
 
-README_FILEPATH = os.path.join(here, 'README.txt')
-CHANGES_FILEPATH = os.path.join(here, 'CHANGES.txt')
+requirements = ['colander',
+                'deform',
+                'couchdbkit']
 
-README = open(README_FILEPATH).read()
-CHANGES = open(CHANGES_FILEPATH).read()
+additional_files = ['README.txt',
+                    'CHANGES.txt']
 
-requirements = ['colander', 'deform', 'couchdbkit']
-additional_files = [README_FILEPATH, CHANGES_FILEPATH]
-
-setup(name='isisdm', version='0.2.1',
+setup(name='isisdm', version='0.2.2',
       packages=find_packages(),
       long_description=README + "\n\n" + CHANGES,
       namespace_packages=['isis'],
@@ -31,6 +34,7 @@ setup(name='isisdm', version='0.2.1',
       download_url='https://github.com/bireme/isisdm',
       license="LGPL v2.1 (http://www.gnu.org/licenses/lgpl-2.1.txt)",
       install_requires=requirements,
+      include_package_data=True,
       test_suite='isis.model',
       tests_require=['Nose'],
       description='ISIS-DM allows programmers to express constraints and functionality similar to those of the CDS/ISIS Field Definition Table and the CDS/ISIS Formatting Language, but in contemporary, object-oriented programming languages.',
